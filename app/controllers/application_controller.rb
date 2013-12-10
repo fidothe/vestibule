be7cc430ca722ae6f5251a895f5d1e125dadd10b
Matt Patterson <matt@reprocessed.org>
2013-12-10 11:21:22 +0100

    Extract the archive notice to a translation string

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,8 +24,8 @@ class ApplicationController < ActionController::Base
   end
 
   def set_archive_mode_warning_if_required
-    if Vestibule.mode_of_operation.mode == :archive
-      flash.now[:archive] = 'This version of Vestibule is <em>read only</em>. It represents an archive of the community effort to produce content for <a href="http://rubymanor.org/4/">Ruby Manor 4</a>.'.html_safe
+    no_one_can(:withdraw, :proposal) do
+      flash.now[:archive] = t('vestibule.archive.notice').html_safe
     end
   end
 end
